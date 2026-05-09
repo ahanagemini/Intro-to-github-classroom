492a7b3baebc5c5949d17ab24681a7a260ccf0a5
ahanagemini <ahanagemini@gmail.com>
2026-05-08 22:07:14 -0400

    Update linalg_lab.py

--- linalg_lab.py
+++ linalg_lab.py
@@ -1,25 +1,21 @@
 import numpy as np
 
-
 def vector_math(a, b):
-    # TODO: Return (sum, difference, dot_product)
+    # TODO: Return a tuple of (a + b, a - b, dot product of a and b)
     return None
 
 def normalize_vector(v):
-    # TODO: Return v / magnitude of v
+    # TODO: Return the unit vector (v / magnitude of v)
     return None
 
 def matrix_ops(A, B):
-    # TODO: Return A @ B
+    # TODO: Return the matrix product of A and B
     return None
 
 def is_orthogonal(v1, v2):
-    # TODO: Return True if dot product is 0, else False
-    # Use np.isclose(result, 0)
+    # TODO: Return True if v1 and v2 are orthogonal, else False
     return None
 
-def rotate_90_viz(v):
-    # TODO: 1. Create 90-deg Rotation Matrix R = [[0, -1], [1, 0]]
-    # TODO: 2. Calculate v_rotated = R @ v
-    
+def rotate_90(v):
+    # TODO: Create the 90-degree Rotation Matrix R and return R @ v
     return None
