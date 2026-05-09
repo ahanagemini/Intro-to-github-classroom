6750ad95e5e23feca1ec5ab8f8be6f2292685d9b
ahanagemini <ahanagemini@gmail.com>
2026-05-08 22:03:36 -0400

    Update and rename data_prep.py to linalg_lab.py

---
+++ linalg_lab.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def vector_math(a, b):
+    # TODO: Return (sum, difference, dot_product)
+    return None
+
+def normalize_vector(v):
+    # TODO: Return v / magnitude of v
+    return None
+
+def matrix_ops(A, B):
+    # TODO: Return A @ B
+    return None
+
+def is_orthogonal(v1, v2):
+    # TODO: Return True if dot product is 0, else False
+    # Use np.isclose(result, 0)
+    return None
+
+def rotate_90_viz(v):
+    # TODO: 1. Create 90-deg Rotation Matrix R = [[0, -1], [1, 0]]
+    # TODO: 2. Calculate v_rotated = R @ v
+    
+    return None
